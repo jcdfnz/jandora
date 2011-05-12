64070b8903b3fed2e520dbda6a3cc005f735ed6d
RodionovDV <RodionovDV@gmail.com>
2011-05-12 14:00:38 +0000

    First draft

---
+++ Stegano/src/org/stegano/loader/Loader.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.stegano.loader;
+
+/**
+ *
+ * @author d.rodionov
+ */
+public interface Loader {
+    
+    public boolean load(String filename);
+}
